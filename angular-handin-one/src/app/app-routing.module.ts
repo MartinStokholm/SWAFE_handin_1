31bb599db49f2826a49ff4d94aba43064a262b7b
MartinStokholmWinTerm <msldk6a@gmail.com>
2023-09-27 19:02:34 +0200

    added components for nav and pages

--- angular-handin-one/src/app/app-routing.module.ts
+++ angular-handin-one/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CreditcardComponent } from './creditcard/creditcard.component';
+import { TransactionsComponent } from "./transactions/transactions.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'CreditCard', component: CreditcardComponent },
+  { path: 'Transactions', component: TransactionsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- angular-handin-one/src/app/app.module.ts
+++ angular-handin-one/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { CreditcardComponent } from './creditcard/creditcard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    TransactionsComponent,
+    CreditcardComponent
   ],
   imports: [
     BrowserModule,
