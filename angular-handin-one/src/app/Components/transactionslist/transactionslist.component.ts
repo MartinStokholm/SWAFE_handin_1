bea0e509ba4c020613eddb36ea4047b70d695bae
MartinStokholmWinTerm <msldk6a@gmail.com>
2023-09-29 09:30:14 +0200

    added remaing scaffolding files

---
+++ angular-handin-one/src/app/Components/transactionslist/transactionslist.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-transactionslist',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './transactionslist.component.html',
+  styleUrls: ['./transactionslist.component.css']
+})
+export class TransactionslistComponent {
+
+}
--- angular-handin-one/src/app/app.module.ts
+++ angular-handin-one/src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
-import { TransactionsComponent } from './transactions/transactions.component';
-import { CreditcardComponent } from './creditcard/creditcard.component';
+import { NavbarComponent } from './Modules/navbar/navbar.component';
+import { HomeComponent } from './Modules/home/home.component';
+import { TransactionsComponent } from './Modules/transactions/transactions.component';
+import { CreditcardComponent } from './Modules/creditcard/creditcard.component';
 
 @NgModule({
   declarations: [
