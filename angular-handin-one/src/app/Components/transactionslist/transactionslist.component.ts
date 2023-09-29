2cbcb3fab094be2e8ef09b0b2230a8f84e09ed7d
MartinStokholmWinTerm <msldk6a@gmail.com>
2023-09-29 16:35:55 +0200

    transactions overview

--- angular-handin-one/src/app/Components/transactionslist/transactionslist.component.ts
+++ angular-handin-one/src/app/Components/transactionslist/transactionslist.component.ts
@@ -1,5 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+export interface Transaction {
+  credit_card: {
+    card_number: number;
+    csc_code: number;
+    cardholder_name: string;
+    expiration_date_month: number;
+    expiration_date_year: number;
+    issuer: string;
+  };
+  uid: string;
+  amount: number;
+  comment: string;
+  date: number;
+  currency: string;
+}
 
 @Component({
   selector: 'app-transactionslist',
@@ -9,5 +25,5 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./transactionslist.component.css']
 })
 export class TransactionslistComponent {
-
+  @Input() transactions: any[] = [];
 }
